1976814ae1bd76f9c9cb323abbc35b1709c31b08
Cierra Ray <cierraray2@gmail.com>
2023-04-18 20:14:32 -0400

    took item out

--- src/routes/ItemRoutes.js
+++ src/routes/ItemRoutes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Routes,Route} from 'react-router-dom';
-import Item from '../components/item/Item'
-import NewItem from '../components/item/NewItem';
+
+
 
 export default function ItemRoutes() {
   return (
     <Routes>
-      <Route index element={<Item />} />
-        <Route path=":id" element={<Item />}/>
-        <Route path="newItem" element={<NewItem />}/>
+      <Route index element={<product />} />
+        <Route path=":id" element={<product />}/>
+        
       
     </Routes>
   )
